bbb91ca780fa68fe06aff7dc202ff302fb197389
planttheidea <tony.quetano@planttheidea.com>
2019-05-31 07:41:43 -0700

    add tests for maxArgs

---
+++ __tests__/maxArgs.ts
@@ -0,0 +1,38 @@
+/* globals describe,expect,it */
+
+// src
+import { createGetInitialArgs } from '../src/maxArgs';
+
+describe('getInitialArgs', () => {
+  it('should return the args themselves when the size is larger than the length', () => {
+    const args = ['foo'];
+
+    const getInitialArgs = createGetInitialArgs(2);
+
+    const result = getInitialArgs(args);
+
+    expect(result).toBe(args);
+  });
+
+  it('should produce a new array for standard sizes', () => {
+    const args = [1, 2, 3, 4, 5, 6, 7];
+
+    for (let index = 0; index < 4; index++) {
+      const getInitialArgs = createGetInitialArgs(index);
+
+      expect(getInitialArgs(args)).toEqual(args.slice(0, index));
+    }
+  });
+
+  it('should create a shallow copy for all non-standard sizes', () => {
+    const args = [1, 2, 3, 4, 5, 6, 7];
+    const size = 6;
+
+    const getInitialArgs = createGetInitialArgs(size);
+
+    const result = getInitialArgs(args);
+
+    expect(result).not.toBe(args);
+    expect(result).toEqual(args.slice(0, size));
+  });
+});
