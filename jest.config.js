103c471499ec8326907acc9b79e0fd4b3958523c
planttheidea <tony.quetano@planttheidea.com>
2020-01-03 14:29:53 +0100

    add test for promise option

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   roots: ['<rootDir>'],
+  setupFiles: ['<rootDir>/jest.init.js'],
   transform: {
     '\\.(js|ts|tsx)$': 'babel-jest',
   },
