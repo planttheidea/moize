7de83360d1db18520709c79fb3928e3bdd0aeba6
Tony Quetano <tony.quetano@planttheidea.com>
2021-04-24 15:16:31 -0700

    get webpack 5 working

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -8,68 +8,74 @@ const ROOT = path.resolve(__dirname, '..');
 const PORT = 3000;
 
 module.exports = {
-  cache: true,
+    cache: true,
 
-  devServer: {
-    contentBase: path.join(ROOT, 'dist'),
-    host: 'localhost',
-    inline: true,
-    lazy: false,
-    noInfo: false,
-    port: PORT,
-    quiet: false,
-    stats: {
-      colors: true,
-      progress: true,
+    devServer: {
+        contentBase: path.join(ROOT, 'dist'),
+        host: 'localhost',
+        inline: true,
+        lazy: false,
+        noInfo: false,
+        port: PORT,
+        quiet: false,
+        stats: {
+            colors: true,
+            progress: true,
+        },
     },
-  },
 
-  devtool: '#source-map',
+    devtool: 'source-map',
 
-  entry: [path.resolve(ROOT, 'DEV_ONLY', 'index.ts')],
+    entry: [path.resolve(ROOT, 'DEV_ONLY', 'index.ts')],
 
-  mode: 'development',
+    mode: 'development',
 
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        include: [path.resolve(ROOT, 'src')],
-        loader: 'eslint-loader',
-        options: {
-          configFile: '.eslintrc',
-          failOnError: true,
-          failOnWarning: false,
-          fix: true,
-          formatter: require('eslint-friendly-formatter'),
-        },
-        test: /\.(js|ts)$/,
-      },
-      {
-        include: [path.resolve(ROOT, 'src'), path.resolve(ROOT, 'DEV_ONLY')],
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: true,
-          plugins: ['@babel/plugin-proposal-class-properties'],
-          presets: ['@babel/preset-react'],
-        },
-        test: /\.(js|ts|tsx)$/,
-      },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                include: [path.resolve(ROOT, 'src')],
+                loader: 'eslint-loader',
+                options: {
+                    configFile: '.eslintrc',
+                    failOnError: true,
+                    failOnWarning: false,
+                    fix: true,
+                    formatter: require('eslint-friendly-formatter'),
+                },
+                test: /\.(js|ts)$/,
+            },
+            {
+                include: [
+                    path.resolve(ROOT, 'src'),
+                    path.resolve(ROOT, 'DEV_ONLY'),
+                ],
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                    plugins: ['@babel/plugin-proposal-class-properties'],
+                    presets: ['@babel/preset-react'],
+                },
+                test: /\.(js|ts|tsx)$/,
+            },
+        ],
+    },
+
+    output: {
+        filename: 'moize.js',
+        library: 'moize',
+        libraryTarget: 'umd',
+        path: path.resolve(ROOT, 'dist'),
+        publicPath: `http://localhost:${PORT}/`,
+        umdNamedDefine: true,
+    },
+
+    plugins: [
+        new webpack.EnvironmentPlugin(['NODE_ENV']),
+        new HtmlWebpackPlugin(),
     ],
-  },
 
-  output: {
-    filename: 'moize.js',
-    library: 'moize',
-    libraryTarget: 'umd',
-    path: path.resolve(ROOT, 'dist'),
-    publicPath: `http://localhost:${PORT}/`,
-    umdNamedDefine: true,
-  },
-
-  plugins: [new webpack.EnvironmentPlugin(['NODE_ENV']), new HtmlWebpackPlugin()],
-
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
 };
