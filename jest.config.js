6d426617556b8ce547447e939dfaac441ba5ac40
Tony Quetano <tony.quetano@planttheidea.com>
2021-08-29 07:52:25 -0700

    upgrade jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  coveragePathIgnorePatterns: ['node_modules', 'src/types.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>'],
-  setupFiles: ['<rootDir>/jest.init.js'],
-  transform: {
-    '\\.(js|ts|tsx)$': 'babel-jest',
-  },
-  testRegex: '/__tests__/.*\\.(ts|tsx|js)$',
-  verbose: true,
+    coveragePathIgnorePatterns: ['node_modules', 'src/types.ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    roots: ['<rootDir>'],
+    setupFiles: ['<rootDir>/jest.init.js'],
+    testEnvironment: 'jsdom',
+    testRegex: '/__tests__/.*\\.(ts|tsx|js)$',
+    transform: {
+        '\\.(js|ts|tsx)$': 'babel-jest',
+    },
+    verbose: true,
 };
